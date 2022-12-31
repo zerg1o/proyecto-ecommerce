69825ba9b0b64fcac9850d8d23788fb81d39ae20
David <davidguiahurtado2@gmail.com>
2022-12-31 03:35:32 -0500

    Epica 4-Sprint 4 David Guia Actualizacion Pedidos

---
+++ app/Order_product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order_product extends Model
+{
+    public function getProductsByOrderAndProduct($order_id,$product_id){
+        return $this->where('order_id',$order_id)
+                    ->where('product_id',$product_id)
+                    ->orderBy('id','desc')->first();
+    }
+
+    public function getProductsByOrder($order_id){
+        return $this->where('order_id',$order_id)
+                    ->orderBy('id','desc')->get();
+    }
+}
